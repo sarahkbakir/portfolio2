6ad3025d1faf7ea1565942c4a713258bf913eafb
Sarah Bakir <sarahbakir.k@gmail.com>
2021-10-25 21:52:42 +0200

    layout changes to profile image

--- components/layout.js
+++ components/layout.js
@@ -33,7 +33,7 @@ export default function Layout({ children, home }) {
               src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={100}
-              width={120}
+              width={130}
               alt={name}
             />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
